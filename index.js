537cab39ec0ad386de7e3727575af31bc0efda7e
Erick Vargas <erickkvargas@gmail.com>
2022-04-06 19:06:21 -0700

    commit tree 694f508109d07a192ef96ea2152ab5528f26def7

--- index.js
+++ index.js
@@ -11,6 +11,50 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//const titleCased = () => {
+  //return tutorials
+//}
+
+// const titleCased = array =>
+// {
+//   array = array.map(element => 
+
+//     element.split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')
+//   )
+//   return array;
+// }
+
+//const titleCased = () => {
+  //array = array.map(element =>element.split 
+  //  )
+//}
+
+// const titleCased = function (){
+//   return tutorials.map((title) => {
+//     return title.split('').map((word) => {
+//       return (word.charAt(0).toUpperCase() + word.slice(1));
+//     }).join('');
+//   });
+// }
+//console.log(titleCased(tutorials));
+
+// const titleCased=function (arrayString){
+//   let mapArray=arrayString.map(function (sentence){
+//   let splitWord=sentence.split(" ")
+//   let titleWord=splitWord.map(word=>word[0].toUpperCase()+word.substring(1))
+//   let joinWord=titleWord.join(" ")
+//   return joinWord;
+//   })
+//   return mapArray;
+// }
+// console.log(titleCased(tutorials))
+
 const titleCased = () => {
-  return tutorials
+  tutorials.map(function (element) {
+    const words = element.split(" ");
+    words.map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    }).join(" ");
+  })
 }
+console.log(titleCased(tutorials));
